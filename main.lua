4f6735a6ade8b6beb3dbd035eab56eb39d42664c
unknown <P305@P305-031.lhu.edu.tw>
2014-11-26 16:27:54 +0800

    word改git

--- main.lua
+++ main.lua
@@ -12,5 +12,5 @@
 
 local background = display.newImage( "world.jpg", display.contentCenterX, display.contentCenterY )
 
-local myText = display.newText( "Hello, World!", display.contentCenterX, display.contentWidth / 4, native.systemFont, 40 )
+local myText = display.newText( "Hello, Git!", display.contentCenterX, display.contentWidth / 4, native.systemFont, 40 )
 myText:setFillColor( 1, 110/255, 110/255 )
